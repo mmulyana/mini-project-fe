685ac794cf0cfa6948ae5b289529e5a89b42aa1b
mmulyana <mulyan.t20@gmail.com>
2024-04-27 09:51:27 +0700

    extend color and box shadow tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        btn: '0 1px 2px 0 rgba(0,0,0,0.12)',
+        dialog: '0 4px 4px 0 rgba(0,0,0,0.08)',
+      },
+      colors: {
+        neutral: {
+          40: '#E0E0E0',
+          50: '#EDEDED',
+          70: '#757575',
+          90: '#404040',
+          100: '#1D1F20',
+        },
+        primary: '#01959F',
+        danger: '#E11428',
+        secondary: '#1D1F20',
+        success: '#43936C',
+      },
+    },
   },
   plugins: [],
 }
